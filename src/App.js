88b9c38b9a4136260b3d9cf55d73a00ef5bbef6f
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-13 14:48:51 +0600

    routing added to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
-
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/users">
+          <Route index element={<List />} />
+          <Route path=":userId" element={<Single />} />
+          <Route path="new" element={<New />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
