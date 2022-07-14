f915b63a187af5025701c601177788ee98eda396
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-14 17:09:14 +0600

    widgets updating

--- src/components/widget/Widget.js
+++ src/components/widget/Widget.js
@@ -1,11 +1,21 @@
-import './widget.scss';
-
-const Widget = () => {
-    return (
-        <div>
-            Widget
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import "./widget.scss";
+const Widget = ({ type }) => {
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">users</span>
+        <span className="counter">25454</span>
+        <span className="link">See all users</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          20%
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Widget;
