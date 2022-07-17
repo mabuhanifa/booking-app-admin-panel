8c532407e39d7cfb43c631435702e38d31257829
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-17 17:51:57 +0600

    datatable updated eith datatablesource

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Datatable from "../../components/datatable/Datatable";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./list.scss";
@@ -9,6 +10,7 @@ const List = () => {
       <Sidebar />
       <div className="listContainer">
         <Navbar />
+        <Datatable/>
       </div>
     </div>
   );
