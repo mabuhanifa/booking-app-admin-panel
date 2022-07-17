54dcab864a30a3363d20f6839f8b0ba37f89204f
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-17 16:56:30 +0600

    table added

---
+++ src/components/table/Table.js
@@ -0,0 +1,11 @@
+import './table.scss';
+
+const Table = () => {
+    return (
+        <div className="table">
+            
+        </div>
+    );
+};
+
+export default Table;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import Chart from "../../components/chart/Chart";
 import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Table from "../../components/table/Table";
 import Widget from "../../components/widget/Widget";
 import "./home.scss";
 
@@ -23,7 +24,7 @@ const Home = () => {
         </div>
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
-          
+          <Table/>
         </div>
       </div>
     </div>
