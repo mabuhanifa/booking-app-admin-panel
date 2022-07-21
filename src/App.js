e21755690de04a1767cca9642e0c4e1602cb4dd9
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-21 23:47:07 +0600

    dark updated

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
-import './style/dark.scss';
+import "./style/dark.scss";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="users">
-            <Route index element={<List />} />
-            <Route path=":userId" element={<Single />} />
-            <Route path="new" element={<New inputs={userInputs} />} />
+    <div className="app dark">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New inputs={userInputs} />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New inputs={userInputs} />} />
+            </Route>
           </Route>
-          <Route path="products">
-            <Route index element={<List />} />
-            <Route path=":productId" element={<Single />} />
-            <Route path="new" element={<New inputs={userInputs} />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -83,7 +83,6 @@ const Sidebar = () => {
       <div className="bottom">
         <div className="colorOption"></div>
         <div className="colorOption"></div>
-        <div className="colorOption"></div>
       </div>
     </div>
   );
