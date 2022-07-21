fbc58db1cd1b00cbf69f614821e2bb927f3fdc9e
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-21 23:18:40 +0600

    dark added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
+import './style/dark.scss';
 function App() {
   return (
     <BrowserRouter>
