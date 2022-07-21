884a7b695a3d2b775303839c32138f24ab54f9f3
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-21 22:17:07 +0600

    new updated with form source

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { userInputs } from "./formSource";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
@@ -14,12 +15,12 @@ function App() {
           <Route path="users">
             <Route index element={<List />} />
             <Route path=":userId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route path="new" element={<New inputs={userInputs} />} />
           </Route>
           <Route path="products">
             <Route index element={<List />} />
             <Route path=":productId" element={<Single />} />
-            <Route path="new" element={<New />} />
+            <Route path="new" element={<New inputs={userInputs} />} />
           </Route>
         </Route>
       </Routes>
